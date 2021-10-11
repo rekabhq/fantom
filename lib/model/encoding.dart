99229e6481f14f702d7955594104e501200d78ad
FatulM <madani_amirreza@yahoo.com>
2021-10-11 14:23:15 +0330

    add encoding model

---
+++ lib/model/encoding.dart
@@ -0,0 +1,21 @@
+part of 'model.dart';
+
+class Encoding {
+  final String? contentType;
+
+  final Map<String, Header>? headers;
+
+  final String? style;
+
+  final bool? explode;
+
+  final bool? allowReserved;
+
+  Encoding({
+    required this.contentType,
+    required this.headers,
+    required this.style,
+    required this.explode,
+    required this.allowReserved,
+  });
+}
