07739883ac64e09e5d5181662ae8d0e405a3ac68
FatulM <madani_amirreza@yahoo.com>
2021-10-11 16:40:46 +0330

    add header model

--- lib/model/header.dart
+++ lib/model/header.dart
@@ -1,4 +1,27 @@
 part of 'model.dart';
 
-/// todo
-class Header {}
+class Header {
+  final bool? required;
+
+  final bool? deprecated;
+
+  final String? style;
+
+  final bool? explode;
+
+  final bool? allowReserved;
+
+  final Schema? schema;
+
+  final Map<String, MediaType>? content;
+
+  const Header({
+    required this.required,
+    required this.deprecated,
+    required this.style,
+    required this.explode,
+    required this.allowReserved,
+    required this.schema,
+    required this.content,
+  });
+}
