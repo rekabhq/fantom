1818c80dafe749eecf0c6fff03d15f33146f9178
FatulM <madani_amirreza@yahoo.com>
2021-10-11 17:16:44 +0330

    fix responses model

--- lib/model/responses.dart
+++ lib/model/responses.dart
@@ -1,9 +1,15 @@
 part of 'model.dart';
 
 class Responses {
+  /// renamed from `default`.
+  /// since this is a keyword in dart.
+  final Response? common;
+
+  /// other key-value pairs
   final Map<String, Response>? fields;
 
   const Responses({
+    required this.common,
     required this.fields,
   });
 }
