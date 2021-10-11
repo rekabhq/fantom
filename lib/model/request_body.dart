e7fdd98ee2d7a2d0ad201ed7412f9b7a36fc9906
Payam Zahedi <pzahedi1995@gmail.com>
2021-10-11 17:10:10 +0330

    remove description from request body

--- lib/model/request_body.dart
+++ lib/model/request_body.dart
@@ -1,14 +1,11 @@
 part of 'model.dart';
 
 class RequestBody {
-  final String? description;
-
   final Map<String, MediaType> content;
 
   final bool? required;
 
   const RequestBody({
-    required this.description,
     required this.content,
     required this.required,
   });
