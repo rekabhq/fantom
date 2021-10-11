a9e6199f9046c23a539632223cbc0b8af6c5d7d8
alireza easazade <easazade@gmail.com>
2021-10-11 11:48:45 +0330

    remove unnecessary utility methods

--- lib/src/extensions/extensions.dart
+++ lib/src/extensions/extensions.dart
@@ -18,14 +18,8 @@ extension IntExt on int {
   Future secondsDelay() async => Future.delayed(Duration(seconds: this));
 
   Duration seconds() => Duration(milliseconds: this);
-  Duration milliseconds() => Duration(milliseconds: this);
-}
 
-extension IntExtNullable on int? {
-  bool get isClientError {
-    if (this == null) return false;
-    return this! >= 400 && this! < 500;
-  }
+  Duration milliseconds() => Duration(milliseconds: this);
 }
 
 extension NullableStringExtensions on String? {
@@ -34,10 +28,10 @@ extension NullableStringExtensions on String? {
   bool get isNotNullOrBlank => !isNullOrBlank;
 
   bool get isValidUrl {
-    if (this != null) {
-      return Uri.tryParse(this!)?.isAbsolute ?? false;
-    } else {
+    if (this == null) {
       return false;
+    } else {
+      return Uri.tryParse(this!)?.isAbsolute ?? false;
     }
   }
 }
@@ -53,51 +47,5 @@ extension StringExtentions on String {
     return result;
   }
 
-  String kb2Mb() {
-    var num = int.tryParse(this);
-    if (num == null) {
-      return this;
-    } else {
-      num = (num / 1000000).floor();
-      return '${num}MB';
-    }
-  }
-
-  String take(int count) {
-    if (length > count) {
-      return substring(0, count);
-    } else {
-      return this;
-    }
-  }
-
-  bool get isValidUrl {
-    if (this == 'null') {
-      return false;
-    }
-    return Uri.parse(this).isAbsolute;
-  }
-
-  bool get isValidEmail =>
-      RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(this);
-
-  int? findFirstNumber() {
-    for (var i = 0; i < length; i++) {
-      var character = this[i];
-      if (isNumeric()) {
-        return int.tryParse(character);
-      }
-    }
-    return null;
-  }
-
-  String get uppercaseFirstLetter {
-    if (isEmpty) {
-      return this;
-    } else {
-      var firstLetter = this[0];
-      var fixed = replaceFirst(firstLetter, firstLetter.toUpperCase());
-      return fixed;
-    }
-  }
+  bool get isValidUrl => Uri.parse(this).isAbsolute;
 }
