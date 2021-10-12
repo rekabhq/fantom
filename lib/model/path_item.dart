cab29ec62bb9631a52d5c84ff6b81e5d0714b9ed
Payam Zahedi <pzahedi1995@gmail.com>
2021-10-12 08:25:37 +0330

    add content of the path item class

--- lib/model/path_item.dart
+++ lib/model/path_item.dart
@@ -1,4 +1,37 @@
 part of 'model.dart';
 
-/// todo
-class PathItem {}
+class PathItem {
+  final String? ref;
+
+  final Operation? get;
+
+  final Operation? put;
+
+  final Operation? post;
+
+  final Operation? delete;
+
+  final Operation? options;
+
+  final Operation? head;
+
+  final Operation? patch;
+
+  final Operation? trace;
+
+  // TODO: check the documentation for the implementing this property
+  final List<Parameter>? parameters;
+
+  const PathItem({
+    required this.ref,
+    required this.get,
+    required this.put,
+    required this.post,
+    required this.delete,
+    required this.options,
+    required this.head,
+    required this.patch,
+    required this.trace,
+    required this.parameters,
+  });
+}
