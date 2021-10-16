92fdca016b0e4211f8b02c432687ca11e69aa443
alireza easazade <easazade@gmail.com>
2021-10-16 17:07:30 +0330

    add a simple test for openapi model schema

---
+++ test/openapi/model/schema/schema_test.dart
@@ -0,0 +1,26 @@
+@Timeout(Duration(minutes: 1))
+import 'dart:io';
+
+import 'package:fantom/src/openapi/model/model.dart';
+import 'package:fantom/src/utils/utililty_functions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  setUp(() async {});
+
+  tearDown(() {});
+
+// TODO: more tests are requried for schema model
+  group('Schema', () {
+    test('should parse order object from order.yaml without any errors',
+        () async {
+      var map = await readJsonOrYamlFile(
+          File('test/openapi/model/schema/order.yaml'));
+      var orderSchema = Schema.fromMap(map);
+      expect(orderSchema.type, map['type']);
+      expect(orderSchema.properties!.length, map['properties'].length);
+      expect(orderSchema.properties!.keys.toSet(),
+          (map['properties'] as Map<String, dynamic>).keys.toSet());
+    });
+  });
+}
