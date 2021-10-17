3c74a51fc8f15efa51c0c9516761d418ab364a74
FatulM <madani_amirreza@yahoo.com>
2021-10-17 15:44:39 +0330

    add switch case for type

--- lib/src/generator/components/schema/schema.dart
+++ lib/src/generator/components/schema/schema.dart
@@ -3,32 +3,32 @@ import 'package:fantom/src/openapi/model/model.dart';
 class SchemaGenerator {
   const SchemaGenerator();
 
-  List<SchemaElement> generate(final Map<String, Schema> schemas) =>
+  List<String> generate(final Map<String, Schema> schemas) =>
       schemas.entries.mapToList(
         (entry) => _generate(entry.key, entry.value),
       );
 
-  SchemaElement _generate(final String className, final Schema schema) {
-    final code = '';
-    return SchemaElement(
-      className: className,
-      code: code,
-    );
+  String _generate(final String className, final Schema schema) {
+    switch (schema.type) {
+      case 'string':
+        throw UnimplementedError();
+      case 'number':
+        throw UnimplementedError();
+      case 'integer':
+        throw UnimplementedError();
+      case 'boolean':
+        throw UnimplementedError();
+      case 'array':
+        throw UnimplementedError();
+      case 'object':
+        throw UnimplementedError();
+      default:
+        throw UnimplementedError();
+    }
   }
 
   String _className(final Reference<Schema> reference) =>
       reference.ref.removeFromStart('#components/schemas/');
-}
-
-class SchemaElement {
-  final String className;
-
-  final String code;
-
-  const SchemaElement({
-    required this.className,
-    required this.code,
-  });
 }
 
 extension on String {
