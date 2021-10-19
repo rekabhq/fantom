ce6f4c6e9df70c44fd6f5c525d505ee948d9c65c
Amirreza Madani <madani_amirreza@yahoo.com>
2021-10-19 16:18:16 +0330

    prepare to push

--- lib/src/generator/components/schema/schema.dart
+++ lib/src/generator/components/schema/schema.dart
@@ -9,40 +9,12 @@ class SchemaGenerator {
     this.compatibilityMode = false,
   });
 
-  List<String> generate(final Map<String, Schema> schemas) =>
-      schemas.entries.mapToList(
-        (entry) => _generate(entry.key, entry.value),
-      );
-
-  String _generate(final String className, final Schema schema) {
-    // switch (schema.type) {
-    //   case 'null':
-    //     throw UnimplementedError();
-    //   case 'string':
-    //     throw UnimplementedError();
-    //   case 'number':
-    //     throw UnimplementedError();
-    //   case 'integer':
-    //     throw UnimplementedError();
-    //   case 'boolean':
-    //     throw UnimplementedError();
-    //   case 'array':
-    //     throw UnimplementedError();
-    //   case 'object':
-    //     if (schema.format != null) throw AssertionError();
-    //     if (schema.defaultValue != null) throw UnimplementedError();
-    //     if (schema.deprecated != null) throw UnimplementedError();
-    //     if (schema.items != null) throw AssertionError();
-    //     if (schema.uniqueItems != null) throw AssertionError();
-    //     throw UnimplementedError();
-    //   default:
-    //     throw UnimplementedError();
-    // }
+  List<String> generate(final Map<String, Schema> schemas) {
     throw UnimplementedError();
   }
 }
 
-extension on String {
+extension StringExt on String {
   /// assert that string starts with given [start],
   /// and remove [start] from start of string.
   String removeFromStart(final String start) {
@@ -53,7 +25,7 @@ extension on String {
   }
 }
 
-extension on Reference<Schema> {
+extension SchemaReferenceExt on Reference<Schema> {
   /// get class name for a schema reference
   String get className => ref.removeFromStart('#components/schemas/');
 }
