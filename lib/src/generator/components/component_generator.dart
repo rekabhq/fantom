1ba81e7b1a3866de5b60bddff925a9ab9abf2738
alireza easazade <easazade@gmail.com>
2021-10-23 11:09:23 +0330

    add base implementation for ComponentGenerator

--- lib/src/generator/components/component_generator.dart
+++ lib/src/generator/components/component_generator.dart
@@ -1,6 +1,27 @@
+import 'package:fantom/src/generator/components/schema/schema.dart';
 import 'package:fantom/src/openapi/model/model.dart';
+import 'package:version/version.dart';
 
 class ComponentsGenerator {
   final OpenApi openApi;
-  ComponentsGenerator({required this.openApi});
+  // TODO: name SchemaComponentGenerator makes more sense
+  final SchemaGenerator schemaGenerator; //other generators are needed as well
+
+  ComponentsGenerator({
+    required this.openApi,
+    required this.schemaGenerator,
+  });
+
+  factory ComponentsGenerator.createDefault(OpenApi openApi) {
+    return ComponentsGenerator(
+      openApi: openApi,
+      schemaGenerator: SchemaGenerator(
+        compatibilityMode: openApi.version.compareTo(Version(3, 1, 0)) < 0,
+      ), // determin compatibility mode from version
+    );
+  }
+
+  void generateAndRegisterComponents() {
+    // TODO : should generate components from [openApi] and register them using registerComponent() method
+  }
 }
