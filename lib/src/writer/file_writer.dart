00c0d70944dee212ee787ba9bde5b8c2f3048446
alireza easazade <easazade@gmail.com>
2021-10-23 10:43:25 +0330

    add integration for Generator & FileWriter to GenerateCommand of cli

--- lib/src/writer/file_writer.dart
+++ lib/src/writer/file_writer.dart
@@ -1,7 +1,9 @@
+import 'package:fantom/src/extensions/extensions.dart';
 import 'package:fantom/src/generator/utils/generation_data.dart';
 
 class FileWriter {
-  final GenerationData generationData;
-
-  FileWriter({required this.generationData});
+  static Future writeGeneratedFiles(GenerationData generationData) async {
+    //TODO: should write the api-classes and models to disk based on generation-data
+    await 2.secondsDelay();
+  }
 }
