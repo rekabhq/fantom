7925e360dc34e2627522dee973f178c79c80dd2a
Payam Zahedi <pzahedi1995@gmail.com>
2021-10-23 19:16:06 +0330

    fix schema generator test error

--- lib/src/generator/components/component_generator.dart
+++ lib/src/generator/components/component_generator.dart
@@ -1,20 +1,20 @@
-import 'package:fantom/src/generator/components/schema/schema.dart';
 import 'package:fantom/src/openapi/model/model.dart';
-import 'package:version/version.dart';
 
 class ComponentsGenerator {
-  final SchemaGenerator schemaGenerator;
+  //TODO: add schema generator as dependency
+  
+  // final SchemaGenerator schemaGenerator;
 
-  ComponentsGenerator({
-    required this.schemaGenerator,
-  });
+  ComponentsGenerator();
 
   factory ComponentsGenerator.createDefault(OpenApi openApi) {
-    return ComponentsGenerator(
-      schemaGenerator: SchemaGenerator(
-        compatibilityMode: openApi.version.compareTo(Version(3, 1, 0)) < 0,
-      ), // determin compatibility mode from version
-    );
+    return ComponentsGenerator();
+    // TODO: update this with schemaGenarator and remove commented codes
+    // return ComponentsGenerator(
+    //   schemaGenerator: SchemaGenerator(
+    //     compatibilityMode: openApi.version.compareTo(Version(3, 1, 0)) < 0,
+    //   ), // determine compatibility mode from version
+    // );
   }
 
   void generateAndRegisterComponents(OpenApi openApi) {
