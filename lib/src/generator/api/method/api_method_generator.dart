08a9db3bec56247156ba82d1ffade69ce5d56190
Payam Zahedi <pzahedi1995@gmail.com>
2021-10-24 14:35:37 +0330

    add structure of api method generator class

--- lib/src/generator/api/method/api_method_generator.dart
+++ lib/src/generator/api/method/api_method_generator.dart
@@ -1,5 +1,7 @@
+import 'package:fantom/src/generator/api/method/method_name_generator.dart';
 import 'package:fantom/src/generator/api/method/params_parser.dart';
 import 'package:fantom/src/generator/api/method/response_parser.dart';
+import 'package:fantom/src/generator/model/operation_detail.dart';
 import 'package:fantom/src/openapi/model/model.dart';
 
 class ApiMethodGenerator {
@@ -13,15 +15,87 @@ class ApiMethodGenerator {
     required this.methodResponseParser,
   });
 
-  String generateMethod(Operation operation) {
-    //TODO: shold use [methodParamsParser] & [methodResponseParser] to find out what are the responses and parameters
-    // for this api method and then create a body accordingly
-    // then return the entire method definition as a String
+  String generateMethods() {
+    if (openApi.paths?.paths.isEmpty ?? true) return '';
 
-    return '''
-    User getUserById(int id){
-      
+    // helper class for generating names
+    final nameGenerator = MethodNameGenerator();
+
+    // buffer to store generated data
+    final buffer = StringBuffer();
+
+    // this is a container that help ous to generate and store unique names for our methods
+    final methodNameHistory = <OperationDetail, String>{};
+
+    // iterating over paths
+    for (final path in openApi.paths!.paths.entries) {
+      final pathParams = path.value.parameters;
+
+      if (path.value.operations.isNotEmpty) {
+        buffer.writeln('//${path.key}');
+      }
+
+      // iterating over operations of the path
+      for (final operation in path.value.operations.entries) {
+        final operationDetail = OperationDetail(
+          path: path.key,
+          operationType: operation.key,
+          operationId: operation.value.operationId,
+        );
+
+        final methodName = nameGenerator.generateUniqueName(
+          operationDetail,
+          methodNameHistory.values.toList(),
+        );
+
+        methodNameHistory[operationDetail] = methodName;
+
+        buffer.writeln(
+          _generateOperation(
+            methodName,
+            operation,
+            pathParams,
+          ),
+        );
+      }
     }
-    ''';
+
+    return buffer.toString();
+  }
+
+  String _generateOperation(
+    String methodName,
+    MapEntry<String, Operation> operation,
+    List<Referenceable<Parameter>>? pathParams,
+  ) {
+    return """
+
+    """;
+  }
+
+  //TODO(payam): update response of this method
+  String _generateParameters(
+    List<Referenceable<Parameter>>? operationParams,
+    List<Referenceable<Parameter>>? pathParams,
+  ) {
+    return """
+
+    """;
+  }
+
+  //TODO(payam): update response of this method
+  String _generateRequestBody(
+    Referenceable<RequestBody>? requestBody,
+  ) {
+    return """
+
+    """;
+  }
+
+  //TODO(payam): update response of this method
+  String _generateResponses(Responses responses) {
+    return """
+
+    """;
   }
 }
