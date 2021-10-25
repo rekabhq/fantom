ca82936c01318b33853656b6ceb0b2d37f8b59e4
Amirreza Madani <madani_amirreza@yahoo.com>
2021-10-25 16:45:54 +0330

    add schema class generator

---
+++ lib/src/generator/schema/schema_class_generator.dart
@@ -0,0 +1,69 @@
+import 'package:fantom/src/generator/utils/string_utils.dart';
+import 'package:fantom/src/mediator/model/schema/schema_model.dart';
+
+class SchemaClassGenerator {
+  const SchemaClassGenerator();
+
+  // todo: default value is not supported
+  String generate(final ObjectDataElement element) {
+    // todo: free form
+    if (element.additionalItems != null) {
+      throw UnimplementedError('free-form objects are not supported');
+    }
+    if (element.name == null) {
+      throw UnimplementedError('anonymous objects are not supported');
+    }
+    for (final property in element.properties) {
+      if (property.item.type == null) {
+        throw UnimplementedError('anonymous inner objects are not supported');
+      }
+    }
+
+    return [
+      'class ${element.name} {',
+      // ...
+      [
+        for (final property in element.properties)
+          [
+            'final ',
+            if (property.isRequired) 'Optional<',
+            property.item.type!,
+            if (property.isRequired) '>?',
+            ' ',
+            property.name,
+            ';',
+          ].joinParts(),
+      ].joinLines(),
+      // ...
+      [
+        '${element.name} ({',
+        // .../...
+        [
+          for (final property in element.properties)
+            [
+              'required ',
+              if (property.isRequired) 'Optional<',
+              property.item.type!,
+              if (property.isRequired) '>?',
+              ' ',
+              property.name,
+              ',',
+            ].joinParts(),
+        ].joinLines(),
+        ')} : ',
+        // .../...
+        [
+          for (final property in element.properties)
+            [
+              property.name,
+              ' = ',
+              property.name,
+              ',',
+            ].joinParts(),
+        ].joinLines().removeFromLast(','),
+        ';'
+      ].joinLines(),
+      '}',
+    ].joinLines();
+  }
+}
