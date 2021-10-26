7f3f9ebf4c0d2f8b820f06cac21c76df8ca78421
Amirreza Madani <madani_amirreza@yahoo.com>
2021-10-26 16:46:27 +0330

    fix tests of mediator

--- test/mediator/schema/schema_mediator_test.dart
+++ test/mediator/schema/schema_mediator_test.dart
@@ -9,9 +9,27 @@ void main() {
   // todo: very incomplete ...
   group('mediator', () {
     group('schema', () {
-      test('sample convert', () async {
+      test('sample convert 3.0', () async {
         final map = await readJsonOrYamlFile(File(
           'openapi_files/petstore.openapi.yaml',
+        ));
+        final openApi = OpenApiReader.parseOpenApiModel(map);
+        final mediator = SchemaMediator(compatibility: true);
+
+        for (final name in openApi.components!.schemas!.keys) {
+          final schema = openApi.components!.schemas![name]!;
+          final element = mediator.convert(
+            openApi: openApi,
+            schema: schema,
+            name: name,
+          );
+          expect(element.name, equals(name));
+        }
+      });
+
+      test('sample convert 3.1', () async {
+        final map = await readJsonOrYamlFile(File(
+          'openapi_files/petstore.openapi-3.1.yaml',
         ));
         final openApi = OpenApiReader.parseOpenApiModel(map);
         final mediator = SchemaMediator(compatibility: false);
