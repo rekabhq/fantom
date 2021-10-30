44ea143cb4e2ac0abc5ac34a39a55264fad89252
alireza easazade <easazade@gmail.com>
2021-10-30 15:41:46 +0330

    complete utility method that generates sealed class from media-types

--- lib/src/utils/sealed_generator_utils.dart
+++ lib/src/utils/sealed_generator_utils.dart
@@ -30,42 +30,51 @@ Future generateSealedTypeFromMediaTypes({
   required String name,
   required Map<String, MediaType> mediaTypes,
   required File output,
-  required GeneratedSchemaComponent Function(Referenceable<Schema> schema)
+  required GeneratedSchemaComponent Function(
+          Referenceable<Schema> schema, String name)
       createGeneratedComponentForSchema,
 }) async {
-  var typeName = ReCase(name).pascalCase;
+  var typeName = '${ReCase(name).pascalCase}RequestBody';
+  var items = List.generate(mediaTypes.entries.length, (index) {
+    var entry = mediaTypes.entries.toList()[index];
+    var subClassTypeName =
+        '${ReCase(name).pascalCase}${ReCase(_getContentTypeShortName(entry.key)).pascalCase}';
+    print(subClassTypeName);
+    var subClassTypeShortName = ReCase(entry.key).camelCase;
+    print(subClassTypeShortName);
+    var mediaType = entry.value;
+    var refOrSchema = mediaType.schema!;
+    late GeneratedSchemaComponent component;
+    if (refOrSchema.isReference) {
+      //TODO schema might be refrenciable in that case we must first retrive schema
+      //and then get our generatedComponent from componentRegistry because it is already registered there
+      throw Exception('WWWWWHATTTTT ???');
+    } else {
+      // our schema object first needs to be generated and registered
+      component = createGeneratedComponentForSchema(
+        refOrSchema,
+        '${ReCase(name).pascalCase}Body${ReCase(_getContentTypeShortName(entry.key)).pascalCase}'
+            .camelCase,
+      );
+    }
+    return ManifestItem(
+      name: subClassTypeName,
+      shortName: subClassTypeShortName,
+      equality: ManifestEquality.identity,
+      fields: [
+        ManifestField(
+          name: ReCase(entry.key).camelCase,
+          type: ManifestType(
+            name: component.dataElement.type!,
+            isNullable: component.dataElement.isNullable,
+          ),
+        )
+      ],
+    );
+  });
   final source = Manifest(
     name: typeName,
-    items: List.generate(mediaTypes.entries.length, (index) {
-      var entry = mediaTypes.entries.toList()[index];
-      var subClassTypeName = '$typeName${ReCase(entry.key).pascalCase}';
-      var subClassTypeShortName = ReCase(entry.key).pascalCase;
-      var mediaType = entry.value;
-      var refOrSchema = mediaType.schema!;
-      late GeneratedSchemaComponent component;
-      if (refOrSchema.isReference) {
-        //TODO schema might be refrenciable in that case we must first retrive schema
-        //and then get our generatedComponent from componentRegistry because it is already registered there
-        throw Exception('WWWWWHATTTTT ???');
-      } else {
-        // our schema object first needs to be generated and registered
-        component = createGeneratedComponentForSchema(refOrSchema);
-      }
-      return ManifestItem(
-        name: subClassTypeName,
-        shortName: subClassTypeShortName,
-        equality: ManifestEquality.identity,
-        fields: [
-          ManifestField(
-            name: component.dataElement.name!,
-            type: ManifestType(
-              name: component.dataElement.type!,
-              isNullable: component.dataElement.isNullable,
-            ),
-          )
-        ],
-      );
-    }),
+    items: items,
     params: [],
     fields: [],
   );
@@ -80,4 +89,24 @@ Future generateSealedTypeFromMediaTypes({
 $contentForward
 
   ''');
+}
+
+String _getContentTypeShortName(String contentType) {
+  var name = contentType;
+  if (contentType == 'application/json') {
+    name = 'Json';
+  } else if (contentType == 'application/xml') {
+    name = 'Xml';
+  } else if (contentType == 'multipart/form-data') {
+    name = 'Multipart';
+  } else if (contentType == 'text/plain') {
+    name = 'TextPlain';
+  } else if (contentType == 'application/x-www-form-urlencoded') {
+    name = 'FormData';
+  } else if (contentType == '*/*') {
+    name = 'Unknown';
+  } else if (contentType.startsWith('image/')) {
+    name = 'Image';
+  }
+  return name;
 }
