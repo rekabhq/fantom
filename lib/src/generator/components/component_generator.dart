e9ac61fbdacd11a896c5eb441d6df47dfd19f9e5
Payam Zahedi <pzahedi1995@gmail.com>
2021-10-30 17:07:31 +0330

    add parameter generator to component generator

--- lib/src/generator/components/component_generator.dart
+++ lib/src/generator/components/component_generator.dart
@@ -1,34 +1,55 @@
 import 'package:fantom/src/generator/components/component/generated_components.dart';
 import 'package:fantom/src/generator/components/components_collection.dart';
+import 'package:fantom/src/generator/parameter/parameter_class_generator.dart';
 import 'package:fantom/src/generator/schema/schema_class_generator.dart';
+import 'package:fantom/src/generator/utils/reference_finder.dart';
 import 'package:fantom/src/mediator/mediator/schema/schema_mediator.dart';
 import 'package:fantom/src/mediator/model/schema/schema_model.dart';
 import 'package:fantom/src/reader/model/model.dart';
 
 class ComponentsGenerator {
   ComponentsGenerator({
-    required this.schemaClassGenerator,
     required this.schemaMediator,
+    required this.schemaClassGenerator,
+    required this.parameterClassGenerator,
   });
 
-  final SchemaClassGenerator schemaClassGenerator;
   final SchemaMediator schemaMediator;
 
+  final SchemaClassGenerator schemaClassGenerator;
+  final ParameterClassGenerator parameterClassGenerator;
+
   factory ComponentsGenerator.createDefault(OpenApi openApi) {
+    final schemaMediator = SchemaMediator();
+    final schemaGenerator = SchemaClassGenerator();
     return ComponentsGenerator(
-      schemaClassGenerator: SchemaClassGenerator(),
-      schemaMediator: SchemaMediator(),
+      schemaMediator: schemaMediator,
+      schemaClassGenerator: schemaGenerator,
+      parameterClassGenerator: ParameterClassGenerator(
+        schemaGenerator: schemaGenerator,
+        schemaMediator: schemaMediator,
+      ),
     );
   }
 
   void generateAndRegisterComponents(OpenApi openApi) {
     List<Map<String, GeneratedComponent>> allGeneratedComponents = [];
 
-    var schemaComponents = _generateSchemas(
+    final schemaComponents = _generateSchemas(
       openApi,
       openApi.components!.schemas!,
     );
-    allGeneratedComponents.addAll([schemaComponents]);
+
+    final parameterComponents = _generateParameters(
+      openApi,
+      openApi.components!.parameters!,
+    );
+
+    allGeneratedComponents.addAll([
+      schemaComponents,
+      parameterComponents,
+    ]);
+    
     for (var map in allGeneratedComponents) {
       map.forEach((ref, component) {
         registerGeneratedComponent(ref, component);
@@ -53,5 +74,25 @@ class ComponentsGenerator {
       }
       return MapEntry(ref, component);
     });
+  }
+
+  Map<String, GeneratedParameterComponent> _generateParameters(
+    OpenApi openApi,
+    Map<String, Referenceable<Parameter>> parameters,
+  ) {
+    final referenceFinder = ReferenceFinder(openApi: openApi);
+    return parameters.map(
+      (key, value) {
+        return MapEntry(
+          key,
+          parameterClassGenerator.generate(
+            openApi,
+            value.isValue
+                ? value.value
+                : referenceFinder.findParameter(value.reference),
+          ),
+        );
+      },
+    );
   }
 }
