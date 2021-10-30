bbc195a44502be88aaaa97e19e0da239bb48f015
alireza easazade <easazade@gmail.com>
2021-10-30 14:27:44 +0330

    skip tests in generate_requestbody_test.dart

--- test/generator/request_body/generate_requestbody_test.dart
+++ test/generator/request_body/generate_requestbody_test.dart
@@ -13,31 +13,34 @@ import 'package:test/test.dart';
 
 void main() {
   group('request_body tests', () {
-    test('test request_body type generation from map of mediaTypes => contents',
-        () async {
-      var map =
-          await readJsonOrYamlFile(File('test/request_body/request_body.json'));
-      var openapiMap = await readJsonOrYamlFile(
-          File('test/request_body/petstore.openapi.json'));
-      var openapi = OpenApi.fromMap(openapiMap);
-      Map<String, MediaType> mediaTypes = map.mapValues(
-        (e) => MediaType.fromMap(e),
-      );
-      await generateSealedTypeFromMediaTypes(
-        name: 'PetRequestBody',
-        mediaTypes: mediaTypes,
-        output: File('test/request_body/output.dart'),
-        createGeneratedComponentForSchema: (schema) {
-          var sm = SchemaMediator();
-          var dataElement = sm.convert(
-              openApi: openapi, schema: schema, name: 'PetRequestBody');
-          if (dataElement is ObjectDataElement) {
-            return SchemaClassGenerator().generate(dataElement);
-          } else {
-            return UnGeneratableSchemaComponent(dataElement: dataElement);
-          }
-        },
-      );
-    });
+    test(
+      'test request_body type generation from map of mediaTypes => contents',
+      () async {
+        var map = await readJsonOrYamlFile(
+            File('test/request_body/request_body.json'));
+        var openapiMap = await readJsonOrYamlFile(
+            File('test/request_body/petstore.openapi.json'));
+        var openapi = OpenApi.fromMap(openapiMap);
+        Map<String, MediaType> mediaTypes = map.mapValues(
+          (e) => MediaType.fromMap(e),
+        );
+        await generateSealedTypeFromMediaTypes(
+          name: 'PetRequestBody',
+          mediaTypes: mediaTypes,
+          output: File('test/request_body/output.dart'),
+          createGeneratedComponentForSchema: (schema) {
+            var sm = SchemaMediator();
+            var dataElement = sm.convert(
+                openApi: openapi, schema: schema, name: 'PetRequestBody');
+            if (dataElement is ObjectDataElement) {
+              return SchemaClassGenerator().generate(dataElement);
+            } else {
+              return UnGeneratableSchemaComponent(dataElement: dataElement);
+            }
+          },
+        );
+      },
+      skip: true,
+    );
   });
 }
