42714119909b17871775d9bafc1bb640193ec33c
Payam Zahedi <pzahedi1995@gmail.com>
2021-10-31 19:47:21 +0330

    update params parser method

--- lib/src/generator/api/method/params_parser.dart
+++ lib/src/generator/api/method/params_parser.dart
@@ -1,15 +1,46 @@
+import 'package:fantom/src/generator/components/component/generated_components.dart';
 import 'package:fantom/src/reader/model/model.dart';
 
 /// should create a metaData object (which is yet to be decided what it is) that explains everything about the parameters
 /// of this method in a way that it can be easily used to generate parameters for our method
 ///
 class MethodParamsParser {
-  List<Arg> parseParams(Operation operation) {
-    //TODO:
+  List<GeneratedParameterComponent> parseParams(
+    List<Referenceable<Parameter>> operationParameters, {
+    List<Referenceable<Parameter>>? pathParameters,
+  }) {
+
     return [];
   }
-}
 
-class Arg {
-  // TODO: should explain everything about a single parameter of an api method like its type, name & is it optional or not
+  List<GeneratedParameterComponent> _parsePureParams(
+    List<Parameter> operationParameters, {
+    List<Parameter>? pathParameters,
+  }) {
+    final uniqueParams = <Parameter>[...operationParameters];
+
+    /// we are finding the path parameters that now override by the operation params and
+    /// add them to [uniqueParams] list
+    if (pathParameters != null) {
+      for (var pathItem in pathParameters) {
+        bool isOverridden = false;
+        for (var uniqueItem in uniqueParams) {
+          if (_isParamOverridden(pathItem, uniqueItem)) {
+            isOverridden = true;
+            break;
+          }
+        }
+
+        if (!isOverridden) {
+          uniqueParams.add(pathItem);
+        }
+      }
+    }
+
+    return [];
+  }
+
+  bool _isParamOverridden(Parameter operation, Parameter path) {
+    return operation.name == path.name && operation.location == path.location;
+  }
 }
