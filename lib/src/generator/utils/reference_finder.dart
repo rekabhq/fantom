f7d87daaaf4552d02f69f33f46825d478ebd5ef0
Payam Zahedi <pzahedi1995@gmail.com>
2021-10-31 11:31:05 +0330

    update implementation of reference finder class

--- lib/src/generator/utils/reference_finder.dart
+++ lib/src/generator/utils/reference_finder.dart
@@ -7,24 +7,18 @@ class ReferenceFinder {
   final OpenApi openApi;
 
   Parameter findParameter(Reference<Parameter> reference) {
-    if (!reference.ref.startsWith('#/components/')) {
-      reference.ref.contains('/parameter/')
-          ? throw AssertionError(
-              'bad reference "${reference.ref}". Parameter reference should placed in #/components/parameter path')
-          : throw AssertionError(
-              'bad reference "${reference.ref}". The reference must be placed in the component section.');
-    }
+    _validateReferencePath(reference, 'parameters', 'Parameter');
 
     if (openApi.components?.parameters == null) {
-      throw AssertionError('Parameters section in component model is empty.');
+      throw AssertionError('Parameters section in component model is empty!');
     }
 
-    final name = reference.ref.removeFromStart('#/components/parameter/');
+    final name = reference.ref.removeFromStart('#/components/parameters/');
 
     final parameter = openApi.components?.parameters![name];
 
     if (parameter == null) {
-      throw AssertionError('Parameter "$name" not found in the component.');
+      throw AssertionError('Parameter "$name" not found in the components.');
     }
 
     return parameter.isValue
@@ -32,13 +26,58 @@ class ReferenceFinder {
         : findParameter(parameter.reference);
   }
 
-  //TODO: impelment this method
   RequestBody findRequestBody(Reference<RequestBody> reference) {
-    throw UnimplementedError();
+    _validateReferencePath(reference, 'requestBodies', 'Request Body');
+
+    if (openApi.components?.requestBodies == null) {
+      throw AssertionError(
+          'RequestBodies section in component model is empty!');
+    }
+
+    final name = reference.ref.removeFromStart('#/components/requestBodies/');
+
+    final requestBody = openApi.components?.requestBodies![name];
+
+    if (requestBody == null) {
+      throw AssertionError('Request Body: "$name" not found in the component.');
+    }
+
+    return requestBody.isValue
+        ? requestBody.value
+        : findRequestBody(requestBody.reference);
   }
 
-  //TODO: impelment this method
   Response findResponse(Reference<Response> reference) {
-    throw UnimplementedError();
+    _validateReferencePath(reference, 'responses', 'Response');
+
+    if (openApi.components?.responses == null) {
+      throw AssertionError('Responses section in component model is empty!');
+    }
+
+    final name = reference.ref.removeFromStart('#/components/responses/');
+
+    final response = openApi.components?.responses![name];
+
+    if (response == null) {
+      throw AssertionError('Request Body: "$name" not found in the component.');
+    }
+
+    return response.isValue ? response.value : findResponse(response.reference);
+  }
+
+  bool _validateReferencePath(
+    Reference reference,
+    String subPath,
+    String componentName,
+  ) {
+    if (!reference.ref.startsWith('#/components/')) {
+      reference.ref.contains('/$subPath/')
+          ? throw AssertionError(
+              'bad reference "${reference.ref}". $componentName reference should placed in #/components/$subPath/ path')
+          : throw AssertionError(
+              'bad reference "${reference.ref}". The reference must be placed in the component section.');
+    }
+
+    return true;
   }
 }
