af29c7a63bb58e79cb8cd19da4afdc9c7015452b
alireza easazade <easazade@gmail.com>
2021-11-02 11:50:53 +0330

    add test for generating response

--- test/generator/response/generated_response_test.dart
+++ test/generator/response/generated_response_test.dart
@@ -4,6 +4,9 @@ import 'dart:io';
 import 'package:fantom/src/generator/components/component_generator.dart';
 import 'package:fantom/src/generator/components/components_registrey.dart';
 import 'package:fantom/src/generator/response/response_class_generator.dart';
+import 'package:fantom/src/generator/schema/schema_class_generator.dart';
+import 'package:fantom/src/mediator/mediator/schema/schema_mediator.dart';
+import 'package:fantom/src/mediator/model/schema/schema_model.dart';
 import 'package:fantom/src/reader/model/model.dart';
 import 'package:fantom/src/utils/utililty_functions.dart';
 
@@ -31,13 +34,35 @@ void main() {
     test(
       'test request_body type generation from map of mediaTypes => contents',
       () async {
-        var response = openapi.components!.responses!.values.first.value;
+        var usersResultResponse =
+            openapi.components!.responses!.values.toList()[2].value;
 
-        var output = responseClassGenerator.generate(response, 'Pet');
+        var output =
+            responseClassGenerator.generate(usersResultResponse, 'UsersResult');
 
         var outputFile = File('test/generator/response/output.dart');
 
         var content = output.fileContent;
+
+        for (final key in openapi.components!.schemas!.keys) {
+          if (key.startsWith('Obj') ||
+              {
+                'Category',
+                'Tag',
+                'User',
+              }.contains(key)) {
+            final schema = openapi.components!.schemas![key]!;
+            final element = SchemaMediator().convert(
+              openApi: openapi,
+              schema: schema,
+              name: key,
+            );
+            final component = SchemaClassGenerator().generate(
+              element as ObjectDataElement,
+            );
+            content += component.fileContent;
+          }
+        }
 
         content += r'''
 class Optional<T> {
