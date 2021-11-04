16df807844ade69240dd3bfeb29dff93906df613
Payam Zahedi <pzahedi1995@gmail.com>
2021-11-04 19:24:21 +0330

    add generated responses to generated collection

--- lib/src/generator/api/method/response_parser.dart
+++ lib/src/generator/api/method/response_parser.dart
@@ -1,4 +1,5 @@
 import 'package:fantom/src/generator/components/component/generated_components.dart';
+import 'package:fantom/src/generator/components/components_registrey.dart';
 import 'package:fantom/src/generator/response/response_class_generator.dart';
 import 'package:fantom/src/reader/model/model.dart';
 
@@ -11,6 +12,15 @@ class MethodResponseParser {
     Responses responses,
     String seedName,
   ) {
-    return responseClassGenerator.generateResponses(responses, seedName);
+    final generatedResponses = responseClassGenerator.generateResponses(
+      responses,
+      seedName,
+    );
+
+    if (generatedResponses.isGenerated) {
+      registerGeneratedComponentWithoutRef(generatedResponses);
+    }
+
+    return generatedResponses;
   }
 }
