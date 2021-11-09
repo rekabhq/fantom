3ff7f69378a537d9103192711b4250f104323e74
Amirreza Madani <madani_amirreza@yahoo.com>
2021-11-09 15:28:48 +0330

    implement schema enum generator

--- lib/src/generator/schema/schema_enum_generator.dart
+++ lib/src/generator/schema/schema_enum_generator.dart
@@ -2,21 +2,16 @@ import 'package:fantom/src/generator/components/component/generated_components.d
 import 'package:fantom/src/generator/schema/schema_value_generator.dart';
 import 'package:fantom/src/generator/utils/string_utils.dart';
 import 'package:fantom/src/mediator/model/schema/schema_model.dart';
-import 'package:meta/meta.dart';
 import 'package:recase/recase.dart';
 
 extension SchemaEnumGeneratorExt on SchemaEnumGenerator {
-  GeneratedSchemaComponent generate(
-    final DataElement element, {
-    required final String name,
-  }) {
+  GeneratedSchemaComponent generate(final DataElement element) {
     return GeneratedSchemaComponent(
       dataElement: element,
       fileContent: generateEnum(
         element,
-        name: name,
       ),
-      fileName: '${ReCase(name).snakeCase}.dart',
+      fileName: '${ReCase(element.enumName).snakeCase}.dart',
     );
   }
 }
@@ -24,154 +19,98 @@ extension SchemaEnumGeneratorExt on SchemaEnumGenerator {
 class SchemaEnumGenerator {
   const SchemaEnumGenerator();
 
-  @visibleForTesting
-  String generateEnum(
-    final DataElement element, {
-    required final String name,
-  }) {
+  String generateEnum(final DataElement element) {
     final enumeration = element.enumeration;
     if (enumeration == null) {
-      throw AssertionError('element ${element.name} does not contain an enum');
+      throw AssertionError(
+        'element ${element.name} does not contain an enum',
+      );
     }
-    final values = enumeration.values;
-    if (values.isEmpty) {
-      // can this happen ?
-      throw AssertionError('element ${element.name} with empty enum');
-    }
+    return _generate(element, enumeration.values);
+  }
+
+  String _generate(
+    final DataElement element,
+    final List<Object?> values,
+  ) {
+    final name = element.enumName;
     final type = element.type;
     final svg = SchemaValueGenerator();
     final names = [
       for (var index = 0; index < values.length; index++) 'value$index',
     ];
     return [
-      'abstract class $name {',
-      '$name._();',
-      for (var index = 0; index < values.length; index++)
-        [
-          'static final $type ',
-          names[index],
-          ' = ',
-          svg.generate(element, value: values[index]),
-          ';',
-        ].joinParts(),
-      for (var index = 0; index < values.length; index++)
-        if (_canSpecific(element, values[index]))
-          [
-            'static final $type ',
-            values[index] == null
-                ? 'valueNull'
-                : [
-                    'value_',
-                    _specific(element, values[index]),
-                  ].joinParts(),
-            ' = ',
-            names[index],
-            ';',
-          ].joinParts(),
+      // enum:
       [
-        'static final List<$type> values = [',
-        names.joinArgsFull(),
-        '];',
-      ].joinParts(),
-      '}',
-    ].joinLines();
-  }
-
-  /// also check types ...
-  bool _canSpecific(
-    final DataElement element,
-    final Object? value,
-  ) {
-    if (value == null) {
-      return true;
-    } else {
-      return element.match(
-        boolean: (boolean) => true,
-        object: (object) => false,
-        array: (array) => false,
-        integer: (integer) => true,
-        number: (number) => true,
-        string: (string) => string.format != StringDataElementFormat.binary,
-        untyped: (untyped) => false,
-      );
-    }
-  }
-
-  String _specific(
-    final DataElement element,
-    final Object? value,
-  ) {
-    if (value == null) {
-      return 'null';
-    } else {
-      return element.match(
-        boolean: (boolean) {
-          if (value is! bool) {
-            throw AssertionError('bad type for enum value');
-          }
-          if (value) {
-            return 'true';
-          } else {
-            return 'false';
-          }
-        },
-        object: (object) {
-          throw AssertionError();
-        },
-        array: (array) {
-          throw AssertionError();
-        },
-        integer: (integer) {
-          if (value is! int) {
-            throw AssertionError('bad type for enum value');
-          }
-          return _int(value);
-        },
-        number: (number) {
-          if (number.isFloat) {
-            if (value is! double) {
-              throw AssertionError('bad type for enum value');
-            }
-            return _double(value);
-          } else {
-            if (value is! num) {
-              throw AssertionError('bad type for enum value');
-            }
-            return _num(value);
-          }
-        },
-        string: (string) {
-          final format = string.format;
-          if (format == StringDataElementFormat.binary) {
-            throw UnimplementedError('only plain string is supported');
-          }
-          if (value is! String) {
-            throw AssertionError('bad type for enum value');
-          }
-          return value;
-        },
-        untyped: (untyped) {
-          throw UnimplementedError('untyped data element');
-        },
-      );
-    }
-  }
-
-  String _int(int value) {
-    return '$value'.replaceAll('-', 'N');
-  }
-
-  String _double(double value) {
-    return '$value'.replaceAll('-', 'N').replaceAll('.', '_');
-  }
-
-  String _num(num value) {
-    if (value is int) {
-      return _int(value);
-    } else if (value is double) {
-      return _double(value);
-    } else {
-      throw AssertionError();
-    }
+        'enum $name {',
+        for (var index = 0; index < values.length; index++)
+          [
+            names[index],
+            ',',
+          ].joinParts(),
+        '}',
+      ].joinLines(),
+      // enum serialization and values class:
+      [
+        'abstract class ${name}Serialization {',
+        // constructor:
+        [
+          '${name}Serialization._() {',
+          'throw AssertionError();',
+          '}',
+        ].joinLines(),
+        // serialize:
+        [
+          'static $type serialize(final $name value) {',
+          'switch(value) {',
+          for (var index = 0; index < values.length; index++)
+            [
+              'case $name.',
+              names[index],
+              ': return ',
+              names[index],
+              ';',
+            ].joinParts(),
+          '}',
+          '}',
+        ].joinLines(),
+        // deserialize:
+        [
+          'static $name deserialize(final $type value) {',
+          for (var index = 0; index < values.length; index++)
+            [
+              'if(_equals(value, ',
+              names[index],
+              ')) return $name.',
+              names[index],
+              ';',
+            ].joinParts(),
+          "throw AssertionError('not found');",
+          '}',
+        ].joinLines(),
+        // value#index:
+        [
+          for (var index = 0; index < values.length; index++)
+            [
+              'static final $type ',
+              names[index],
+              ' = ',
+              svg.generate(element, value: values[index]),
+              ';',
+            ].joinParts(),
+        ].joinLines(),
+        // values:
+        [
+          'static final List<$type> values = [',
+          for (var index = 0; index < values.length; index++)
+            [
+              names[index],
+              ',',
+            ].joinParts(),
+          '];',
+        ].joinLines(),
+        '}',
+      ].joinMethods(),
+    ].joinMethods();
   }
 }
