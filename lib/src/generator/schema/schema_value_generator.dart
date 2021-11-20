5186be269531f5b8be192530018fc1270061ed06
Amirreza Madani <madani_amirreza@yahoo.com>
2021-11-20 14:03:30 +0330

    progress schema value generator towards new enum system

--- lib/src/generator/schema/schema_value_generator.dart
+++ lib/src/generator/schema/schema_value_generator.dart
@@ -235,6 +235,29 @@ class SchemaValueGenerator {
       } else {
         return false;
       }
+    } else if (o1 is Set<Object?> && o2 is Set<Object?>) {
+      if (o1.length == o2.length) {
+        final acc = Set.of(o2);
+        for (final item1 in o1) {
+          bool isFound = false;
+          Object? found;
+          for (final item2 in acc) {
+            if (_deepEquals(item1, item2)) {
+              isFound = true;
+              found = item2;
+              break;
+            }
+          }
+          if (isFound) {
+            acc.remove(found);
+          } else {
+            return false;
+          }
+        }
+        return true;
+      } else {
+        return false;
+      }
     } else if (o1 is Map<String, Object?> && o2 is Map<String, Object?>) {
       if (o1.length == o2.length) {
         for (var key in o1.keys) {
